9dc656fcd93840d91667c4e491a56eb0e1e3b517
Kishore <kishore22705@gmail.com>
2025-02-18 00:18:45 +0530

    first commit

---
+++ components/Header/Header.tsx
@@ -0,0 +1,86 @@
+import gsap from "gsap";
+import { useWindowScroll } from "react-use";
+import { useEffect, useRef, useState } from "react";
+import { TiLocationArrow } from "react-icons/ti";
+import Link from "next/link";
+import Button from "../ui/Button";
+
+const navItems = ["About", "Events", "Contact"];
+
+
+const NavBar = () => {
+  const navContainerRef = useRef<HTMLDivElement>(null);
+  const { y: currentScrollY } = useWindowScroll();
+  const [isNavVisible, setIsNavVisible] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  useEffect(() => {
+    if (!navContainerRef.current) return;
+
+    if (currentScrollY === 0) {
+      setIsNavVisible(true);
+      navContainerRef.current.classList.remove("floating-nav");
+      navContainerRef.current.classList.add("text-b");
+    } else if (currentScrollY > lastScrollY) {
+      setIsNavVisible(false);
+      navContainerRef.current.classList.add("floating-nav");
+      navContainerRef.current.classList.remove("text-b");
+    } else if (currentScrollY < lastScrollY) {
+      setIsNavVisible(true);
+      navContainerRef.current.classList.add("floating-nav");
+      navContainerRef.current.classList.remove("text-b");
+    }
+
+    setLastScrollY(currentScrollY);
+  }, [currentScrollY, lastScrollY]);
+
+  useEffect(() => {
+    if (!navContainerRef.current) return;
+    
+    gsap.to(navContainerRef.current, {
+      y: isNavVisible ? 0 : -100,
+      opacity: isNavVisible ? 1 : 0,
+      duration: 0.2,
+    });
+  }, [isNavVisible]);
+
+  return (
+    <div
+      ref={navContainerRef}
+      className="fixed inset-x-0 top-4 z-50 h-16 border-none transition-all duration-700 sm:inset-x-6"
+    >
+      <header className="absolute top-1/2 w-full -translate-y-1/2">
+        <nav className="flex size-full items-center justify-between p-4">
+          <div className="flex items-center gap-7">
+            <img src="/imgs/favicon.png" alt="logo" className="w-10" />
+
+            <Button
+              link="https://maps.app.goo.gl/9nZRZWoZEKBZESRX7"
+              id="product-button"
+              title="Location"
+              rightIcon={<TiLocationArrow />}
+              leftIcon={null}
+              containerClass="bg-blue-50 md:flex hidden items-center justify-center gap-1"
+            />
+          </div>
+
+          <div className="flex h-full items-center">
+            <div className="hidden md:block">
+              {navItems.map((item, index) => (
+                <Link
+                  key={index}
+                  href={`/${item.toLowerCase()}`}
+                  className="nav-hover-btn"
+                >
+                  {item}
+                </Link>
+              ))}
+            </div>
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default NavBar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,13 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        zentry: ["zentry", "sans-serif"],
+        general: ["general", "sans-serif"],
+        "circular-web": ["circular-web", "sans-serif"],
+        "robert-medium": ["robert-medium", "sans-serif"],
+        "robert-regular": ["robert-regular", "sans-serif"],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
