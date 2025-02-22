ed6785987c8c1616c466d6aec03f27447c214e4f
Kishore <kishore22705@gmail.com>
2025-02-22 14:49:44 +0530

    US

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['gsap'],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: ['assets.aceternity.com'],
   },
